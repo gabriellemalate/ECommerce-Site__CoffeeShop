ba5965d8eaa0a9382754ec050d82b8779be15326
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-14 06:29:11 -0700

    Menu init

---
+++ src/pages/MenuPage/MenuPage.js
@@ -0,0 +1,15 @@
+import "./MenuPage.scss"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function MenuPage() {
+
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default MenuPage;
