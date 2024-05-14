cbd73d9b548c10ccb72417a56463b68c96e63d01
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-14 06:33:34 -0700

    routes for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
+import React, { useState, useEffect }  from 'react';
+import axios from "axios";
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+// import './App.scss';
+
+// import Header from './components/Header/Header';
+import LandingPage from './pages/LandingPage/LandingPage';
+import MenuPage from "./pages/MenuPage/MenuPage";
+import ShopPage from "./pages/ShopePage/ShopePage";
+// import Success from './components/Success/Success';
+// import NotFound from './components/NotFound/Notfound';
 
 function App() {
-  return (
-    <div className="App">
-      <header>
 
-      </header>
-    </div>
+  return (
+    <>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/upload' element={<MenuPage />} />
+          <Route path='/sign' element={<ShopPage />} />
+          {/* <Route path='/success' element={<Success />} /> */}
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
