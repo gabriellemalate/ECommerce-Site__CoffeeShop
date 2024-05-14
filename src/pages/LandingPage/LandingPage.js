652402839d4adc02bfe8385256db5509e4329508
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-14 06:26:33 -0700

    Landing init

---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import "./LandingPage.scss"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function LandingPage() {
+
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default LandingPage;
