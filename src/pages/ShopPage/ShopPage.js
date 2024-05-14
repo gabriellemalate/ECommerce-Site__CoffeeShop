d78b0e61a2e9bbbe3deee12f9851d8d7a894a930
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-14 06:29:48 -0700

    Shop init

---
+++ src/pages/ShopPage/ShopPage.js
@@ -0,0 +1,15 @@
+import "./ShopPage.scss"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function ShopPage() {
+
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default ShopPage;
