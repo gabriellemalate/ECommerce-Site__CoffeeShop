70fb940837e648e1d52f809d4dd03ac49bf7464c
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-15 09:40:12 -0700

    footer style bug fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,17 +13,17 @@ function Footer() {
                     <h3 className="footer-head">Address</h3>
                     <a href="https://www.google.com/maps/dir//503+Broadway,+New+York,+NY+10012/@40.7224818,-74.0819769,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89c259894ee631e7:0x7b0a31ea522821ee!2m2!1d-73.9995769!2d40.7225111?entry=ttu"
                         title="Directions">
-                        <p style="margin-bottom: 0">503 Broadway Penthouse</p>
-                        <p style="margin-top: 0">New York</p>
+                        <p className="footer__par--top">503 Broadway Penthouse</p>
+                        <p className="footer__par--bottom">New York</p>
                     </a>
                 </div>
                 <div className="footer__hours">
                     <h3 className="footer-head">Hours</h3>
-                    <p style="margin-bottom: 0">
+                    <p className="footer__par--top">
                         <strong>Monday - Friday</strong>
                         <time> 9am - 7pm </time>
                     </p>
-                    <p style="margin-top: 0">
+                    <p className="footer__par--bottom">
                         <strong>Saturday - Sunday</strong>
                         <time> 10am - 5pm </time>
                     </p>
