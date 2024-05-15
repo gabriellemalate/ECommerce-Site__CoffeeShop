18c0ccb54319ef12b734757e4c587d49dab4064f
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-15 06:28:01 -0700

    product carousel init

---
+++ src/components/ProductCarousel/ProductCarousel.js
@@ -0,0 +1,15 @@
+import "./ProductCarousel.scss"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function ProductCarousel() {
+
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default ProductCarousel;
