c7196382a1ba83f4284ea75e3d63f415285dc5fc
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-15 06:22:14 -0700

    jsx compatible

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -8,6 +8,23 @@ function LandingPage() {
 
     return (
         <>
+            <main className="hero" id="back-to-top">
+                <article className="hero-items">
+                    <h2>Nitro Coffee</h2>
+                    <p>
+                        Coffee is a brewed drink prepared from roasted coffee beans, the seeds
+                        of berries from certain Coffee species.
+                    </p>
+
+                    <a href="./pages/menu.html" className="a__button">
+                        <div id="button-border"></div>
+                        <button className="hero-items__shop-button">SHOP NOW</button>
+                    </a>
+                </article>
+                <div className="hero-items__hero-image">
+                    <img src="./assets/images/coffeebeans-hero.png" />
+                </div>
+            </main>
         </>
     );
 }
