9451ebc2e001a2eb9299ccc7705096e198db8ecf
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-15 08:57:21 -0700

    sassify css

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -11,25 +11,25 @@ function LandingPage() {
 
     return (
         <>
-        <Header/>
+            <Header />
             <main className="hero" id="back-to-top">
                 <article className="hero-items">
-                    <h2>Nitro Coffee</h2>
-                    <p>
+                    <h2 className="hero-items__head">Nitro Coffee</h2>
+                    <p className="hero-items__detail">
                         Coffee is a brewed drink prepared from roasted coffee beans, the seeds
                         of berries from certain Coffee species.
                     </p>
 
-                    <Link to="/shop" className="a__button">
+                    <Link to="/shop" className="hero-items__link">
                         <div id="button-border"></div>
                         <button className="hero-items__shop-button">SHOP NOW</button>
                     </Link>
                 </article>
                 <div className="hero-items__hero-image">
-                    <img src={Hero}/>
+                    <img src={Hero} alt ="bag of coffeebeans" className="hero-items__hero-image-img"/>
                 </div>
             </main>
-            <ProductCarousel/>
+            <ProductCarousel />
         </>
     );
 }
