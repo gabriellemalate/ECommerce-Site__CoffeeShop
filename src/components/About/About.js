bbd8a739c3778f76e4bac8c66fa950cbe686116c
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-16 07:37:23 -0700

    bem naming

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,21 +8,21 @@ function About() {
 
     return (
         <>
-            <section class="were-different">
-                <div class="were-different-text">
+            <section class="about">
+                <div class="about-text">
                     <h2>We're Different</h2>
                     <p>
                         We grow and roast our own beans. Every bean, every cup, every step of
                         the way. We strive for excellence.
                     </p>
-                    <div class="were-different-text__image-row">
+                    <div class="about-text__image-row">
                         <img src="./assets/images/planting.svg" />
                         <img src="./assets/images/coffee-beans.svg" />
                         <img src="./assets/images/coffee-cup.svg" />
                     </div>
                     <a class="a__button" href="#back-to-top">
                         <div id="button-border"></div>
-                        <button class="were-different__button">LEARN MORE</button>
+                        <button class="about__button">LEARN MORE</button>
                     </a>
                 </div>
 
@@ -30,7 +30,7 @@ function About() {
                     <div class="can-box">
                         <img class="tilted-can" src="./assets/images/tiltedcan.png" />
                     </div>
-                    <div class="were-different-can"></div>
+                    <div class="about-can"></div>
                 </article>
             </section>
         </>
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -30,6 +30,8 @@ function LandingPage() {
                 </div>
             </main>
             <ProductCarousel />
+            <About/>
+            <Discounts/>
         </>
     );
 }
