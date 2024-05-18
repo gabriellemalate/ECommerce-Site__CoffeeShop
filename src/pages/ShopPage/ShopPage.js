f4efc7c970cb167b9069562ac69099b198bd3fa5
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-18 09:41:50 -0700

    shop nav

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -8,6 +8,22 @@ function ShopPage() {
 
     return (
         <>
+            <nav className="shop-nav" id="shop-nav">
+                <ul className="shop-nav__list">
+                    <div className="shop-line"></div>
+                    <li className="shop-nav__list-item">
+                        <a href="#shop-nav"> HOT DRINKS </a>
+                    </li>
+                    <div className="shop-line"></div>
+                    <li className="shop-nav__list-item">
+                        <a href="#coldDrinks"> COLD DRINKS </a>
+                    </li>
+                    <div className="shop-line"></div>
+                    <li className="shop-nav__list-item">
+                        <a href="#pastries"> PASTRIES </a>
+                    </li>
+                </ul>
+            </nav>
         </>
     );
 }
